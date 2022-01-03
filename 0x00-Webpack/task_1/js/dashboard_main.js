04848db6605e688706b3f19c6ab2316e1906211a
Miguel Pacheco <minipachru@gmail.com>
2022-01-03 13:55:44 -0300

    fix js file

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,4 +1,5 @@
-import $ from 'jquery';
+import $ from "jquery";
+import _ from 'lodash';
 
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
@@ -6,11 +7,11 @@ $('body').append('<button>Click here to get started</button>');
 $('body').append("<p id='count'></p>");
 $('body').append('<p>Copyright - Holberton School</p>');
 
-
 let count = 0;
 
 function updateCounter() {
   count += 1;
   $('#count').text(`${count} clicks on the button`);
 }
+
 $('button').on('click', _.debounce(updateCounter, 500));
