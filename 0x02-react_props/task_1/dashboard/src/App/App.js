a2bf242c8dd34bba0da8f03b5cfe743f87d820f1
Miguel Pacheco <minipachru@gmail.com>
2022-01-06 10:45:34 -0300

    add task 0 task 1 and task 2

--- 0x02-react_props/task_1/dashboard/src/App/App.js
+++ 0x02-react_props/task_1/dashboard/src/App/App.js
@@ -1,9 +1,9 @@
 import React, { Fragment } from 'react';
 import './App.css';
-import Header from '../Header/Header.js';
-import Login from '../Login/Login.js';
-import Footer from '../Footer/Footer.js';
-import Notifications from '../Notifications/Notifications.js';
+import Header from '../Header/Header';
+import Login from '../Login/Login';
+import Footer from '../Footer/Footer';
+import Notifications from '../Notifications/Notifications';
 
 function App() {
   return (
