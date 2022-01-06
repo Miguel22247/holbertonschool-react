77e56947c27a4341cc945bc0bb641c9b8571cc28
Miguel Pacheco <minipachru@gmail.com>
2022-01-06 11:32:35 -0300

    fix tasks

--- 0x02-react_props/task_2/dashboard/src/App/App.js
+++ 0x02-react_props/task_2/dashboard/src/App/App.js
@@ -1,21 +1,23 @@
 import React, { Fragment } from 'react';
 import './App.css';
 import Header from '../Header/Header';
-import Login from '../Login/Login';
 import Footer from '../Footer/Footer';
+import Login from '../Login/Login';
 import Notifications from '../Notifications/Notifications';
 
 function App() {
-  return (
-    <Fragment>
-      <Notifications />
-      <div className="App">
-        <Header />
-        <Login />
-        <Footer />
-      </div>
-    </Fragment>
-  );
-}
-
-export default App;
+    return (
+      <Fragment>
+        <Notifications />
+        <div className='App'>
+          <Header />
+          <div className='App-body'>
+            <Login />
+          </div>
+          <Footer />
+        </div>
+      </Fragment>
+    );
+  }
+  
+  export default App;
