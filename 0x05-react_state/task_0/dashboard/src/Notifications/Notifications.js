b02909c07ae8eea9e549604c77394a5d158c7db2
Miguel Pacheco <minipachru@gmail.com>
2022-01-24 09:11:59 -0300

    add task 0

--- 0x05-react_state/task_0/dashboard/src/Notifications/Notifications.js
+++ 0x05-react_state/task_0/dashboard/src/Notifications/Notifications.js
@@ -6,65 +6,65 @@ import closeIcon from '../assets/close-icon.png';
 import NotificationItemShape from './NotificationItemShape';
 
 class Notifications extends React.Component {
-	constructor(props) {
-		super(props);
-		this.markAsRead = this.markAsRead.bind(this);
-	}
+  constructor(props) {
+    super(props);
+    this.markAsRead = this.markAsRead.bind(this);
+  }
 
-	shouldComponentUpdate(newProps) {
-		return (
-			newProps.listNotifications.length > this.props.listNotifications.length ||
-			newProps.displayDrawer !== this.props.displayDrawer
-		);
-	}
+  shouldComponentUpdate(newProps) {
+    return (
+      newProps.listNotifications.length > this.props.listNotifications.length ||
+      newProps.displayDrawer !== this.props.displayDrawer
+    );
+  }
 
-	markAsRead(id) { 
-		console.log(`Notification ${id} has been marked as read`)
-	}
+  markAsRead(id) { 
+    console.log(`Notification ${id} has been marked as read`)
+  }
 
-	render() {
-		const { displayDrawer, listNotifications, handleDisplayDrawer, handleHideDrawer } = this.props;
-		const displayMenu = displayDrawer ? styles.noDisplay : styles.menuItem;
-		return (
-			<React.Fragment>
-				<div id='menuItem' className={css(displayMenu)} onClick={handleDisplayDrawer}>
-					Your notifications
-				</div>
-				{ displayDrawer &&
-					<div className={css(styles.notifications)}>
-						<button id='closeButton' style={{background: 'transparent', float: 'right'}} aria-label='Close' onClick={handleHideDrawer} >
-							<img src={closeIcon} alt="close-icon" className={css(styles.notificationsButton)} />
-						</button>
-						<p>Here is the list of notifications</p>
-						<ul className={css(styles.notificationsUl)}>
-							{
-								listNotifications.length === 0 &&
-									(<NotificationItem
-										value="No new notification for now"
-									/>)
-							}
-							{
-								listNotifications && listNotifications.map((newNote) => (
-									<NotificationItem
-										key={newNote.id}
-										html={newNote.html}
-										type={newNote.type}
-										value={newNote.value}
-										markAsRead={this.markAsRead}
-										id={newNote.id}
-									/>
-								))
-							}
-						</ul>
-					</div>
-				}
-			</React.Fragment>
-		);
-	}
+  render() {
+    const { displayDrawer, listNotifications, handleDisplayDrawer, handleHideDrawer } = this.props;
+    const displayMenu = displayDrawer ? styles.noDisplay : styles.menuItem;
+    return (
+      <React.Fragment>
+        <div id='menuItem' className={css(displayMenu)} onClick={handleDisplayDrawer}>
+          Your notifications
+        </div>
+        { displayDrawer &&
+          <div className={css(styles.notifications)}>
+            <button id='closeButton' style={{background: 'transparent', float: 'right'}} aria-label='Close' onClick={handleHideDrawer} >
+              <img src={closeIcon} alt="close-icon" className={css(styles.notificationsButton)} />
+            </button>
+            <p>Here is the list of notifications</p>
+            <ul className={css(styles.notificationsUl)}>
+              {
+                listNotifications.length === 0 &&
+                  (<NotificationItem
+                    value="No new notification for now"
+                  />)
+              }
+              {
+                listNotifications && listNotifications.map((newNote) => (
+                  <NotificationItem
+                    key={newNote.id}
+                    html={newNote.html}
+                    type={newNote.type}
+                    value={newNote.value}
+                    markAsRead={this.markAsRead}
+                    id={newNote.id}
+                  />
+                ))
+              }
+            </ul>
+          </div>
+        }
+      </React.Fragment>
+    );
+  }
 }
 
 const responsive = {
-	small: '@media screen and (max-width: 900px)'
+  small: '@media screen and (max-width: 900px)'
 };
 
 const opacity = {
@@ -96,78 +96,78 @@ const translateY = {
 };
 
 const styles = StyleSheet.create({
-	menuItem: {
-		fontFamily: 'Arial, Helvetica, sans-serif',
-		textAlign: 'right',
-		fontWeight: 'bold',
-		padding: '5px',
-		paddingRight: '20px',
-		backgroundColor: '#fff8f8',
-		display: 'block',
-		":hover": {
-			cursor: 'pointer',
-			animationName: [opacity, translateY],
-			animationDuration: '1s, 0.5s',
-			animationIterationCount: 3
-		}
-	},
+  menuItem: {
+    fontFamily: 'Arial, Helvetica, sans-serif',
+    textAlign: 'right',
+    fontWeight: 'bold',
+    padding: '5px',
+    paddingRight: '20px',
+    backgroundColor: '#fff8f8',
+    display: 'block',
+    ":hover": {
+      cursor: 'pointer',
+      animationName: [opacity, translateY],
+      animationDuration: '1s, 0.5s',
+      animationIterationCount: 3
+    }
+  },
 
-	noDisplay: {
-		display: 'none'
-	},
+  noDisplay: {
+    display: 'none'
+  },
 
-	notifications: {
-		fontFamily: 'Arial, Helvetica, sans-serif',
-		border: '2px dashed #e1354b',
-		paddingRight: '20px 5px',
-		margin: '5px',
-		position: 'fixed',
-		right: '1%',
-		left: '65%',
-		[responsive.small]: {
-			display: 'block',
-			background: 'white',
-			fontSize: '20px',
-			width: '100vw',
-			height: '100vh',
-			border: 'none',
-			listStyle: 'none',
-			padding: 0,
-			margin: 0,
-			top: 0,
-			right: 0,
-			left: 0,
-		}
-	},
+  notifications: {
+    fontFamily: 'Arial, Helvetica, sans-serif',
+    border: '2px dashed #e1354b',
+    paddingRight: '20px 5px',
+    margin: '5px',
+    position: 'fixed',
+    right: '1%',
+    left: '65%',
+    [responsive.small]: {
+      display: 'block',
+      background: 'white',
+      fontSize: '20px',
+      width: '100vw',
+      height: '100vh',
+      border: 'none',
+      listStyle: 'none',
+      padding: 0,
+      margin: 0,
+      top: 0,
+      right: 0,
+      left: 0,
+    }
+  },
 
-	notificationsButton: {
-		width: '20px',
-		border: 'none',
-		background: 'none',
-		cursor: 'pointer'
-	},
+  notificationsButton: {
+    width: '20px',
+    border: 'none',
+    background: 'none',
+    cursor: 'pointer'
+  },
 
-	notificationsUl: {
-		paddingTop: '15px',
-		paddingLeft: '40px',
-		[responsive.small]: {
-			padding: 0
-		}
-	}
+  notificationsUl: {
+    paddingTop: '15px',
+    paddingLeft: '40px',
+    [responsive.small]: {
+      padding: 0
+    }
+  }
 });
 
 Notifications.propTypes = {
-	displayDrawer: PropTypes.bool,
-	listNotifications: PropTypes.arrayOf(NotificationItemShape),
-	handleDisplayDrawer: PropTypes.func,
-	handleHideDrawer: PropTypes.func
+  displayDrawer: PropTypes.bool,
+  listNotifications: PropTypes.arrayOf(NotificationItemShape),
+  handleDisplayDrawer: PropTypes.func,
+  handleHideDrawer: PropTypes.func
 };
 
 Notifications.defaultProps = {
-	displayDrawer: false,
-	listNotifications: [],
-	handleDisplayDrawer: () => {},
-	handleHideDrawer: () => {}
+  displayDrawer: false,
+  listNotifications: [],
+  handleDisplayDrawer: () => {},
+  handleHideDrawer: () => {}
 };
 
 export default Notifications;
--- 0x05-react_state/task_0/dashboard/src/Notifications/Notifications.test.js
+++ 0x05-react_state/task_0/dashboard/src/Notifications/Notifications.test.js
@@ -29,20 +29,6 @@ describe('Basic React Tests - <Notifications />', function() {
 		expect(wrapper.find('NotificationItem')).toBeTruthy();
 	});
 
-	// it('Should render the right html into the first NotificationItem element', () => {
-	// 	const wrapper = shallow(<Notifications displayDrawer={true} listNotifications={listNotifications} />);
-	// 	expect(wrapper.find('NotificationItem').first().html()).toEqual(
-	// 		'<li data-notification-type="default">New course available</li>'
-	// 	);
-	// });
-
-	// it('Should render the correct string', () => {
-	// 	const wrapper = shallow(<Notifications displayDrawer={true} />);
-	// 	expect(wrapper.find('.Notifications p').text()).toEqual(
-	// 		('Here is the list of notifications')
-	// 	);
-	// });
-
 	it('When displayDrawer is false - Should check that the menu item is being displayed', () => {
 		const wrapper = shallow(<Notifications displayDrawer={false} />);
 		expect(wrapper.find('.menuItem')).toBeTruthy();
@@ -58,10 +44,6 @@ describe('Basic React Tests - <Notifications />', function() {
 		expect(wrapper.find('.menuItem')).toBeTruthy();
 	});
 
-	// it('When displayDrawer is true - Should check that the div.Notifications is not being displayed', () => {
-	// 	const wrapper = shallow(<Notifications displayDrawer={true} />);
-	// 	expect(wrapper.find('div.Notifications')).toHaveLength(1);
-	// });
 });
 
 describe('Basic React Tests - <Notifications list />', function() {
@@ -70,17 +52,6 @@ describe('Basic React Tests - <Notifications list />', function() {
 		{id: 2, type: 'urgent', value: 'New resume available'},
 		{id: 3, type: 'urgent', html: { __html: getLatestNotification() }}
 	];
-
-	// it('Should check that Notifications renders correctly if pass an empty array', () => {
-	// 	const newArray = [];
-	// 	const wrapper = shallow(<Notifications displayDrawer listNotifications={newArray} />);
-	// 	expect(wrapper.find('NotificationItem').html()).toEqual('<li data-notification-type="default" class="blue_1tsdo2i">No new notification for now</li>');
-	// });
-
-	// it('Should check that Notifications renders correctly if don’t pass the listNotifications property', () => {
-	// 	const wrapper = shallow(<Notifications displayDrawer={true} />);
-	// 	expect(wrapper.find('NotificationItem').html()).toEqual('<li data-notification-type="default" class="blue_1tsdo2i">No new notification for now</li>');
-	// });
 
 	it('When pass list of notifications - Should check that renders it correctly and with the right number of NotificationItem', () => {
 		const wrapper = shallow(<Notifications displayDrawer listNotifications={listNotifications} />);
